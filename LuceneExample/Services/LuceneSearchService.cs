be47d0416e9a9a30d0e7ad7066e02cfb3baa8b1f
Alim <Alim@DESKTOP-6LJPRKV>
2025-06-29 17:30:03 +0330

    Add count arg to SearchProducts method

--- LuceneExample/Services/LuceneSearchService.cs
+++ LuceneExample/Services/LuceneSearchService.cs
@@ -12,17 +12,17 @@ namespace LuceneExample.Services
         private static readonly string LuceneIndexDirectory = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "LuceneIndex");
         private static readonly LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
 
-        public List<Guid> SearchProducts(string searchText)
+        public List<Guid> SearchProducts(string searchText, int count)
         {
             var directory = FSDirectory.Open(LuceneIndexDirectory);
             using var reader = DirectoryReader.Open(directory);
             var searcher = new IndexSearcher(reader);
 
             using var analyzer = new StandardAnalyzer(AppLuceneVersion);
-            var parser = new MultiFieldQueryParser(AppLuceneVersion, new[] { "Title", "TaxRate" }, analyzer);
+            var parser = new MultiFieldQueryParser(AppLuceneVersion, new[] { "Title", "Description" }, analyzer);
 
             var query = parser.Parse(searchText);
-            var hits = searcher.Search(query, 500).ScoreDocs;
+            var hits = searcher.Search(query, count).ScoreDocs;
 
             return hits.Select(hit => Guid.Parse(searcher.Doc(hit.Doc).Get("Id"))).ToList();
         }
